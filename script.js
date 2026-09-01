3b78979b7886154fcd13cbf23c74bf29dba52a83
chauhandharmik2503 <chauhandharmik785@gmail.com>
2026-09-01 14:54:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -177,12 +177,13 @@ displayMenu();
 function filter(category)
 {
     var items = document.getElementsByClassName("food-item");
+
     for (var i = 0; i < items.length; i++)
     {
         if (category === "all" ||
             items[i].classList.contains(category))
         {
-            items[i].style.display = "";
+            items[i].style.display = "flex";
         }
         else
         {
